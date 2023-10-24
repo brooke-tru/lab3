8e094940b125dfb98fe458d26da08c6dc501f0f0
brooke-tru <bmtruong@ucsd.edu>
2023-10-24 09:17:31 -0700

    Fix ArrayExamples.java and new tests for ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+	@Test 
+	public void testReverseInPlace1() {
+    int[] input1 = { 2, 4, 6 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 6, 4, 2 }, input1);
+	}
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = { 1, 3, 5 };
+    assertArrayEquals(new int[]{ 5, 3, 1 }, ArrayExamples.reversed(input1));
+  }
+
 }
